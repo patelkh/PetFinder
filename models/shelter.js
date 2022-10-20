e7bcd7b4321f951feeb41785f469805dd91af019
Kay Patel <patelkh@oregonstate.edu>
2022-10-20 08:43:53 -0700

    Added font-awesome icons

--- models/shelter.js
+++ models/shelter.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ShelterSchema = new Schema({
-    name: {type: String, rquired: true},
+    name: {type: String, required: true},
     location: {type: Schema.Types.ObjectId, ref: "Location"}
 });
 
